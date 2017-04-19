b7fbf067acee39a19de0610a65be93cb4f757f33
kheaton <kwillheat@gmail.com>
2017-04-18 21:59:46 -0600

    Some scaffolding

--- sobel.cu
+++ sobel.cu
@@ -5,7 +5,17 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-	printf("this is working\n");
+
+	// *Optional* - call ffmpeg to split up video
+	// pull image files
+	// setup cuda parameters (splitting up blocks warps ect)
+	
+	// https://devblogs.nvidia.com/parallelforall/how-overlap-data-transfers-cuda-cc/
+	// Async push images to GPU, then process, then pull back processed images to host
+
+	// save processed images
+
+	// *Optional* - call ffmpeg to stitch images back into video
 
 	return 0;
 }
